6346f0ee5e7df7894583d91b53ac55734104e11c
NoName <53113607+JonasNorlander@users.noreply.github.com>
2024-01-15 09:50:05 +0100

    feat: STREAMLIT GUI, APP LOGIC, CALCULATION, run_app()
    module/file: main.py
    
    run_app()
        * Streamlit gui, skapar gui element
        * App logic, hämntar data från input, anropar funktioner för
        beräkning. st.write - output, visar antalet lvl 1 items som behövs
        för att nå target level.
    
    Färdig version

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from typing import List
 
 
@@ -13,18 +14,15 @@ def level_1_needed(target_level: int) -> int:
     return 2 ** (target_level - 1)
 
 
-# level_1_needed(2)
-
 def existing_level_1_used(items: List[int]) -> int:
     """
-        existing_level_1_used beräknar hur många lvl 1 som behövs redan finns totalt i listan av 'items'
+        existing_level_1_used beräknar hur många lvl 1 som redan finns totalt i listan av 'items'
         Itererar över [items] i en for-loop och anropar 'level_1_needed()' för att beräkna hur många level 1 som använts för varje item. 
-        Summerar ihop detta i 'total_used' vilket retuneras i funktionen 
 
         Args:
-            items (List[int]): från input vilken level varje existerande item har 
+            items (List[int]): från input vilken level varje existerande item har.
         Returns:
-            int: totala antalet level 1 som redan finns i alla items. 
+            int: Summerat värde av totala antalet level 1 som redan finns i alla items.
     """
     total_used = 0
 
@@ -34,5 +32,23 @@ def existing_level_1_used(items: List[int]) -> int:
     return total_used
 
 
-# existing_level_1_used([2,5,8])
+def run_app() -> None:
+    st.title("Level 1 counter")
 
+    target_level = st.number_input("Target level :dart:", min_value=1, max_value=20, value=4,
+                                   help="Set target level, max value: 50")
+
+    items_level_str = st.text_input("Existing items level, SEPERATE WITH ' , ' (comma) ")
+
+    if st.button("Calculate"):
+        items_list = [int(number) for number in items_level_str.split(",")] if items_level_str else []
+
+        calculated_target_level = level_1_needed(target_level)
+        calculated_items_total = existing_level_1_used(items_list)
+
+        st.write(calculated_target_level - calculated_items_total)
+
+
+if __name__ == "__main__":
+    # run_app()
+    print(type(existing_level_1_used([2,4,5])))
